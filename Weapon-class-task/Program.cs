71dbfc3c0d179d7c3ceb678d61b21a7658875eeb
coderILKIN <tu758403c@code.edu.az>
2022-05-15 09:00:56 +0400

    first

---
+++ Weapon-class-task/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Weapon_class_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletcapacity;
+            int bulletcount;
+            bool check = true;
+
+            do
+            {
+                Console.WriteLine("Enter Bulletcapacity:");
+
+                bulletcapacity = Convert.ToInt32(Console.ReadLine());
+            } while (bulletcapacity < 0 || bulletcapacity == 0);
+
+
+            do
+            {
+                Console.WriteLine("Enter Bulletcount:");
+                bulletcount = Convert.ToInt32(Console.ReadLine());
+
+            } while (bulletcount < 0 || bulletcount>bulletcapacity || bulletcount == 0);
+
+            Weapon weapon = new Weapon(bulletcapacity,bulletcount);
+
+            weapon.Bulletcount = bulletcapacity;
+            weapon.Bulletcount = bulletcount;
+
+            do
+            {
+                Console.WriteLine("0: İnformasiya almaq üçün");
+                Console.WriteLine("1: Shoot metodu üçün");
+                Console.WriteLine("2: Fire metodu üçün");
+                Console.WriteLine("3: GetRemainBulletCount metodu üçün");
+                Console.WriteLine("4: Reload metodu üçün");
+                Console.WriteLine("5: ChangeFireMode metodu üçün");
+                Console.WriteLine("6: Proqramdan dayandırmaq üçün");
+
+                switch (Console.ReadLine())
+                {
+                    case "0":
+                        break;
+                    case "1":
+                        weapon.Shoot();
+                        break;
+                    case "2":
+                        weapon.Fire();
+                        break;
+                    case "3":
+                        Console.WriteLine(weapon.GetRemainBulletCount());
+                        break;
+                    case "4":
+                        weapon.Reload();
+                        break;
+                    case "5":
+                        weapon.ChangeFire();
+                        break;
+                    case "6":
+                        check = false;
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (check);
+        }
+    }
+}
---
+++ Weapon-class-task/Weapon.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weapon_class_task
+{
+    internal class Weapon
+    {
+
+        public bool isAutomatic;
+
+        public Weapon(int bulletcapacity, int bulletcount)
+        {
+            Bulletcapacity = bulletcapacity;
+            Bulletcount = bulletcount;
+        }
+
+
+        private int _bulletcapacity;
+        public int Bulletcapacity { 
+            get 
+            {
+                return _bulletcapacity;
+
+            }
+            set
+            {
+                if (value>0)
+                {
+                    _bulletcapacity = value;
+                }
+            }
+        }
+
+        private int _bulletcount;
+
+
+        public int Bulletcount { 
+            get 
+            {
+                return _bulletcount;
+            } 
+            set 
+            {
+                if (value>0)
+                {
+                    _bulletcount = value;
+                }
+            } 
+        }
+
+
+        public void Shoot()
+        {
+            if (Bulletcount>0)
+            {
+                Console.WriteLine(--Bulletcount);
+            }
+            else
+            {
+                Console.WriteLine("Gulle yoxdur");
+            }
+        }
+
+
+        public void Fire()
+        {
+            if (Bulletcount>0)
+            {
+                if (isAutomatic)
+                {
+                    Console.WriteLine(Bulletcapacity = 0);
+                }
+                else
+                {
+                    Console.WriteLine(--Bulletcount);
+                }
+            }
+        }
+
+
+        public int GetRemainBulletCount()
+        {
+            if (Bulletcapacity != Bulletcount)
+            {
+                return Bulletcapacity - Bulletcount;
+            }
+            return Bulletcount;
+        }
+
+        public void Reload()
+        {
+            int count;
+            if (Bulletcount != Bulletcapacity)
+            {
+                count = Bulletcapacity - Bulletcount;
+                Bulletcount += count;
+                Console.WriteLine(Bulletcount);
+            }
+            else
+            {
+                Console.WriteLine("Do not Reload");
+            }
+        }
+
+
+        public void ChangeFire()
+        {
+            if (isAutomatic == true)
+            {
+                isAutomatic = false;
+
+                Console.WriteLine("Firemode" + isAutomatic);
+            }
+            else
+            {
+                isAutomatic = true;
+                Console.WriteLine("Firemode" + isAutomatic);
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
